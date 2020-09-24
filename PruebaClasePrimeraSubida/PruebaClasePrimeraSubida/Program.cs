d5c5a5303e5d52eda6e31e48d99259f9148585f6
PeioCode4Jobs <71778831+PeioCode4Jobs@users.noreply.github.com>
2020-09-24 16:27:29 +0200

    Primer Commit

---
+++ PruebaClasePrimeraSubida/PruebaClasePrimeraSubida/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaClasePrimeraSubida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
